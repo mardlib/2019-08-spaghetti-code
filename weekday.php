eb786d904add4ffbf02110b1c29227a82da117c4
Martin Sovinz <martin.dibek@icloud.com>
2019-08-29 15:43:39 +0200

    added function main void

--- weekday.php
+++ weekday.php
@@ -4,51 +4,89 @@
  * Wochentagsberechnung nach https://de.wikipedia.org/wiki/Wochentagsberechnung
  */
 
-setlocale(LC_TIME, 'de_AT.utf-8');
-
-$day = $argv[1];
-$month = $argv[2];
-$year = $argv[3]; /* muss vierstellig sein */
-
-if($argc<4 || $argc>5) {
-    echo "Wrong number of arguments.";
-    exit(1);
+/**
+ * @param int $w
+ * @return mixed
+ */
+function getWeekdayName(int $w)
+{
+    $weekDayNames = [
+        "Sonntag",
+        "Montag",
+        "Diensttag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Samstag",
+    ];
+    $weekday = $weekDayNames[$w];
+    return $weekday;
+}
+/**
+ * @param $day
+ * @param $month
+ * @param $year
+ */
+function printEingabe($day, $month, $year): void
+{
+    echo "Eingabe: {$day}.{$month}.{$year}\n";
+}
+/**
+ * @param $year
+ * @param $month
+ * @param $day
+ */
+function printAusgabe($year, $month, $day): void
+{
+    echo strftime("Berechnung PHP: Wochentag='%A'\n", strtotime("$year-$month-$day"));
+}
+/**
+ * @param $weekday
+ * @param int $m
+ * @param $y
+ * @param $c
+ */
+function printDebugOutput($weekday, int $m, $y, $c): void
+{
+    global $argc;
+    global $argv;
+    echo "Berechnung Algorithmus: Wochentag='{$weekday}'\n";
+    if ($argc > 4 && ($argv[4] == '-d' || $argv[4] == '--debug')) {
+       echo "DEBUG: m={$m} y={$y} c={$c}\n";
+    }
 }
 
-$d = $day;
-$m = (($month - 2 - 1 ) + 12 ) % 12 + 1 ; // this is because of the modulo
-$c = substr($year, 0, 2);
-if($m>=11) {
-    $c = substr($year-1, 0, 2);
-}
-$y = substr($year, 2, 2);
-if($m>=11) {
-    $y = substr($year-1, 2, 2);
+function main(): void
+{
+    setlocale(LC_TIME, 'de_AT.utf-8');
+
+    $day = $argv[1];
+    $month = $argv[2];
+    $year = $argv[3]; /* muss vierstellig sein */
+
+    if ($argc < 4 || $argc > 5) {
+        echo "Wrong number of arguments.";
+        exit(1);
+    }
+
+    $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
+    $c = substr($year, 0, 2);
+    if ($m >= 11) {
+        $c = substr($year - 1, 0, 2);
+    }
+    $y = substr($year, 2, 2);
+    if ($m >= 11) {
+        $y = substr($year - 1, 2, 2);
+    }
+
+    $weekdayNumber = ($day + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
+
+    $weekday = getWeekdayName($weekdayNumber);
+
+    printEingabe($day, $month, $year);
+    printAusgabe($year, $month, $day);
+    printDebugOutput($weekday, $m, $y, $c);
 }
 
-$w = ($d + intval (2.6 * $m - 0.2) + $y  + intval ($y/4) + intval ($c/4) - 2*$c ) % 7;
-if($w == 1) {
-    $x = "Montag";
-} elseif($w == 2) {
-    $x = "Dienstag";
-} elseif($w == 3) {
-    $x = "Mittwoch";
-} elseif($w == 4) {
-    $x = "Donnerstag";
-} elseif($w == 5) {
-    $x = "Freitag";
-} elseif($w == 6) {
-    $x = "Samstag";
-} elseif($w == 0) {
-    $x = "Sonntag";
-} else {
-    echo "Error: Unknown w={$w}\n";
-    exit(1);
-}
-echo "Eingabe: {$day}.{$month}.{$year}\n";
-echo strftime("Berechnung PHP: Wochentag='%A'\n",strtotime("$year-$month-$day"));
-echo "Berechnung Algorithmus: Wochentag='{$x}'\n";
-if($argc>4 && ( $argv[4]=='-d' || $argv[4]=='--debug')) {
-    echo "DEBUG: m={$m} y={$y} c={$c}\n";
-}
+main();
 
